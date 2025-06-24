b0c02b8854cf18d090f5b4c9e13831a2bb43791f
Ha Nguyen Thao Vy <hhnnttvy@gmail.com>
2025-06-24 09:41:53 +0700

    Update page.js

--- nextapp/src/app/page.js
+++ nextapp/src/app/page.js
@@ -13,6 +13,9 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+         <li className="tracking-[-.01em]">
+            Hello, this is my app demo.
+          </li>
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
