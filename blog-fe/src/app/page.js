71a782994e679013d424879262c4e0fcf83178f4
Ha Nguyen Thao Vy <hhnnttvy@gmail.com>
2025-06-26 16:58:30 +0700

    test

--- blog-fe/src/app/page.js
+++ blog-fe/src/app/page.js
@@ -4,7 +4,7 @@ import posts from './data/posts'
 export default function Home() {
    return (
     <main className="p-8 bg-gray-50 min-h-screen">
-      <h1 className="text-3xl font-bold mb-6 text-red-500">📝 Welcome to My Demo Blog</h1>
+      <h1 className="text-3xl font-bold mb-6 text-red-500">Welcome to My Demo Blog</h1>
       <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {posts.map((post) => (
           <BlogCard key={post.id} post={post} />
